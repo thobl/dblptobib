7489454605988a15d864e771a37437596783b159
Thomas Bläsius <thomas.blaesius@kit.edu>
2021-09-16 09:03:02 +0200

    output only the specified papers + cli outputs bib or html

--- dblptobib.py
+++ dblptobib.py
@@ -6,8 +6,17 @@
 
 import sys
 
+import data
+import parse_dblp
+import output
+import output_html
+import output_bib
+
+# parsing the user input
 if len(sys.argv) != 3:
-    print("Usage: ./dblptobib.py input output")
+    print("Usage: ./dblptobib.py <input> <output>")
+    print("<input> must be a file containing paper ids")
+    print("<output> specifies the output file")
     sys.exit()
 
 input_arg = sys.argv[1]
@@ -16,12 +25,13 @@ output_arg = sys.argv[2]
 with open(input_arg) as f:
     papers = [line.rstrip() for line in f]
 
-import data
-import parse_dblp
-import output
-import output_html
-import output_bib
-
+if output_arg.endswith(".bib"):
+    output_type = output_bib
+elif output_arg.endswith(".html"):
+    output_type = output_html
+else:
+    print("<output> must be a *.bib or a *.html file")
+    sys.exit()
 
 # make sure local directories exist
 data.setup_directories()
@@ -34,4 +44,4 @@ for paper_id in papers:
 parse_dblp.get_venues()
 
 # some output
-output.write(output.Output(output_bib, filename=output_arg))
+output.write(output.Output(output_type, paper_ids=papers, filename=output_arg))
